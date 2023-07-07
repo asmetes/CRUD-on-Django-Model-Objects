c30e7ee25a8c0c6250c50f16f4f291cec90c0542
asmetes <asmextesfex@gmail.com>
2023-07-07 03:57:30 -0400

    Second add view and template

--- Views-and-Templates/lab3_template/myproject/urls.py
+++ Views-and-Templates/lab3_template/myproject/urls.py
@@ -21,5 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('onlinecourse/', include('onlinecourse.urls')),
+    # Add the following line to match the root URL
+    path('', include('onlinecourse.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
